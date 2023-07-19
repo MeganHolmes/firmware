031ec7be5af9dd10143d2822ee924d208ca086dc
Megan Holmes <meganholmes100@gmail.com>
2023-07-19 13:43:30 -0700

    Modify FreeRTOSConfig.h

--- src/processorSpecific/ST/STM32F01/RTOS/FreeRTOSConfig.h
+++ src/processorSpecific/ST/STM32F01/RTOS/FreeRTOSConfig.h
@@ -1,20 +1,22 @@
 // Note: This file is from https://www.freertos.org/a00110.html and the default
-// values are used except where explicitly noted as "non-default" below.
+// values are used except where explicitly noted as "Non-default" below.
+// Non-default flags are used in case I break anything and can narrow down
+// potential causes.
 
 #ifndef FREERTOS_CONFIG_H
 #define FREERTOS_CONFIG_H
 
 #define configUSE_PREEMPTION                    1
-#define configUSE_PORT_OPTIMISED_TASK_SELECTION 0
+#define configUSE_PORT_OPTIMISED_TASK_SELECTION 1 // Non-default
 #define configUSE_TICKLESS_IDLE                 0
-#define configCPU_CLOCK_HZ                      60000000
-#define configSYSTICK_CLOCK_HZ                  1000000
-#define configTICK_RATE_HZ                      250
-#define configMAX_PRIORITIES                    5
+#define configCPU_CLOCK_HZ                      72000000 // Non-default
+// #define configSYSTICK_CLOCK_HZ                  1000000 // Non-default. Undefined so it uses the same as configCPU_CLOCK_HZ
+#define configTICK_RATE_HZ                      100 // Non-default
+#define configMAX_PRIORITIES                    4 // Non-default
 #define configMINIMAL_STACK_SIZE                128
 #define configMAX_TASK_NAME_LEN                 16
 #define configUSE_16_BIT_TICKS                  0
-#define configTICK_TYPE_WIDTH_IN_BITS           TICK_TYPE_WIDTH_16_BITS
+#define configTICK_TYPE_WIDTH_IN_BITS           TICK_TYPE_WIDTH_32_BITS // Non-default
 #define configIDLE_SHOULD_YIELD                 1
 #define configUSE_TASK_NOTIFICATIONS            1
 #define configTASK_NOTIFICATION_ARRAY_ENTRIES   3
@@ -24,7 +26,7 @@
 #define configUSE_ALTERNATIVE_API               0 /* Deprecated! */
 #define configQUEUE_REGISTRY_SIZE               10
 #define configUSE_QUEUE_SETS                    0
-#define configUSE_TIME_SLICING                  0
+#define configUSE_TIME_SLICING                  1 // Non-default
 #define configUSE_NEWLIB_REENTRANT              0
 #define configENABLE_BACKWARD_COMPATIBILITY     0
 #define configNUM_THREAD_LOCAL_STORAGE_POINTERS 5
@@ -34,22 +36,22 @@
 #define configHEAP_CLEAR_MEMORY_ON_FREE         1
 
 /* Memory allocation related definitions. */
-#define configSUPPORT_STATIC_ALLOCATION             1
+#define configSUPPORT_STATIC_ALLOCATION             0 // Non-default
 #define configSUPPORT_DYNAMIC_ALLOCATION            1
 #define configTOTAL_HEAP_SIZE                       10240
-#define configAPPLICATION_ALLOCATED_HEAP            1
-#define configSTACK_ALLOCATION_FROM_SEPARATE_HEAP   1
+#define configAPPLICATION_ALLOCATED_HEAP            0 // Non-default
+#define configSTACK_ALLOCATION_FROM_SEPARATE_HEAP   0 // Non-default
 
 /* Hook function related definitions. */
 #define configUSE_IDLE_HOOK                     0
 #define configUSE_TICK_HOOK                     0
 #define configCHECK_FOR_STACK_OVERFLOW          0
-#define configUSE_MALLOC_FAILED_HOOK            0
+#define configUSE_MALLOC_FAILED_HOOK            0 // Default. Not needed for now but might be useful later
 #define configUSE_DAEMON_TASK_STARTUP_HOOK      0
 #define configUSE_SB_COMPLETED_CALLBACK         0
 
 /* Run time and task stats gathering related definitions. */
-#define configGENERATE_RUN_TIME_STATS           0
+#define configGENERATE_RUN_TIME_STATS           0 // Default. Not needed for now but might be useful later
 #define configUSE_TRACE_FACILITY                0
 #define configUSE_STATS_FORMATTING_FUNCTIONS    0
 
@@ -58,27 +60,28 @@
 #define configMAX_CO_ROUTINE_PRIORITIES         1
 
 /* Software timer related definitions. */
-#define configUSE_TIMERS                        1
+#define configUSE_TIMERS                        0 // Non-default. I don't think I need this if I use HW timers.
 #define configTIMER_TASK_PRIORITY               3
 #define configTIMER_QUEUE_LENGTH                10
 #define configTIMER_TASK_STACK_DEPTH            configMINIMAL_STACK_SIZE
 
 /* Interrupt nesting behaviour configuration. */
-// #define configKERNEL_INTERRUPT_PRIORITY         [dependent of processor]
-// #define configMAX_SYSCALL_INTERRUPT_PRIORITY    [dependent on processor and application]
+// If I broke something it's probably here because STM32 M3 is special
+#define configKERNEL_INTERRUPT_PRIORITY         4
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY    1
 // #define configMAX_API_CALL_INTERRUPT_PRIORITY   [dependent on processor and application]
 
 /* Define to trap errors during development. */
 #define configASSERT( ( x ) ) if( ( x ) == 0 ) vAssertCalled( __FILE__, __LINE__ )
 
 /* FreeRTOS MPU specific definitions. */
-#define configINCLUDE_APPLICATION_DEFINED_PRIVILEGED_FUNCTIONS 0
+#define configINCLUDE_APPLICATION_DEFINED_PRIVILEGED_FUNCTIONS 0 // Default. Don't need an MPU probably ever
 #define configTOTAL_MPU_REGIONS                                8 /* Default value. */
 #define configTEX_S_C_B_FLASH                                  0x07UL /* Default value. */
 #define configTEX_S_C_B_SRAM                                   0x07UL /* Default value. */
-#define configENFORCE_SYSTEM_CALLS_FROM_KERNEL_ONLY            1
+#define configENFORCE_SYSTEM_CALLS_FROM_KERNEL_ONLY            0 // Non-default.
 #define configALLOW_UNPRIVILEGED_CRITICAL_SECTIONS             1
-#define configENABLE_ERRATA_837070_WORKAROUND                  1
+#define configENABLE_ERRATA_837070_WORKAROUND                  0 // Non-default. Not using an M4
 #define configUSE_MPU_WRAPPERS_V1                              0
 #define configPROTECTED_KERNEL_OBJECT_POOL_SIZE                10
 #define configSYSTEM_CALL_STACK_SIZE                           128
