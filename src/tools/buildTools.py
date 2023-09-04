190a33c809ee47961662ec7f00da6901932fd74b
Megan Holmes <meganholmes100@gmail.com>
2023-09-04 10:35:03 -0700

    Fix multiple define error, still have load seg warning, project compiles

--- src/tools/buildTools.py
+++ src/tools/buildTools.py
@@ -1,16 +1,15 @@
-def processFeature(feature, featuresEnabled, featureList, verbose):
+def processFeature(feature, featuresEnabled, sources, featureList, verbose):
     if verbose:
         print(f"  Processing feature {feature}")
 
     if feature not in featuresEnabled:
         featuresEnabled.append(feature)
 
-    sources = []
-
     if feature in featureList:
         for source in featureList[feature]['SOURCES']:
-            sources.append(source)
-            if verbose:
+            if source not in sources:
+                sources.append(source)
+                if verbose:
                     print(f"    Adding source file {source}")
 
         for dependency in featureList[feature]['DEPENDENCIES']:
@@ -19,8 +18,7 @@ def processFeature(feature, featuresEnabled, featureList, verbose):
                 if verbose:
                     print(f"    Enabling dependency {dependency}")
 
-                featuresEnabled, newSources = processFeature(dependency, featuresEnabled, featureList, verbose)
-                sources.extend(newSources)
+                featuresEnabled, sources = processFeature(dependency, featuresEnabled, sources, featureList, verbose)
     else:
         pass
         # TODO: save this info somewhere and pass it back. We can later use this to make sure that all features have been found somewhere
