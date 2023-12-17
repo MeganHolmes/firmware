cff5230fa1c8641a90fabd4832453b67fd7378d7
Megan Holmes <meganholmes100@gmail.com>
2023-12-17 12:32:29 -0800

    Add basic assembly and linker files, fix sconscript linker error

---
+++ src/processorSpecific/ST/STM32F10/startup/core.s
@@ -0,0 +1,31 @@
+// These instructions define attributes of our chip and
+// the assembly language we'll use:
+.syntax unified
+.cpu cortex-m3
+.fpu softvfp
+.thumb
+
+// Global memory locations.
+.global reset_handler
+
+/*
+ * The Reset handler. Called on reset.
+ */
+.type reset_handler, %function
+reset_handler:
+  // Set the stack pointer to the end of the stack.
+  // The '_estack' value is defined in our linker script.
+  LDR  r0, =_estack
+  MOV  sp, r0
+
+  // Set some dummy values. When we see these values
+  // in our debugger, we'll know that our program
+  // is loaded on the chip and working.
+  LDR  r7, =0xDEADBEEF
+  MOVS r0, #0
+  main_loop:
+    // Add 1 to register 'r0'.
+    ADDS r0, r0, #1
+    // Loop back.
+    B    main_loop
+.size reset_handler, .-reset_handler
--- src/projectSpecific/blinky/blinky.c
+++ src/projectSpecific/blinky/blinky.c
@@ -8,37 +8,37 @@
 // #include "app_led.h"
 // #include "app_led_heartbeat.h"
 
-#include "stm32f10x_gpio.h"
+// #include "stm32f10x_gpio.h"
 
 int main(void)
 {
     // HW_GPIO_init();
     // app_led_init();
     uint64_t counter = 0;
-    GPIO_InitTypeDef gpio_init;
-    GPIO_StructInit(&gpio_init);
-    gpio_init.GPIO_Mode = GPIO_Mode_Out_PP;
+    // GPIO_InitTypeDef gpio_init;
+    // GPIO_StructInit(&gpio_init);
+    // gpio_init.GPIO_Mode = GPIO_Mode_Out_PP;
 
-    GPIO_Init(GPIOA, &gpio_init);
-    GPIO_Init(GPIOB, &gpio_init);
-    GPIO_Init(GPIOC, &gpio_init);
-    GPIO_Init(GPIOD, &gpio_init);
-    GPIO_Init(GPIOE, &gpio_init);
-    GPIO_Init(GPIOF, &gpio_init);
-    GPIO_Init(GPIOG, &gpio_init);
+    // GPIO_Init(GPIOA, &gpio_init);
+    // GPIO_Init(GPIOB, &gpio_init);
+    // GPIO_Init(GPIOC, &gpio_init);
+    // GPIO_Init(GPIOD, &gpio_init);
+    // GPIO_Init(GPIOE, &gpio_init);
+    // GPIO_Init(GPIOF, &gpio_init);
+    // GPIO_Init(GPIOG, &gpio_init);
     while (true)
     {
         counter++;
 
-        // if (counter > 100000000 == 0)
-        {
-            GPIO_WriteBit(GPIOA, GPIO_Pin_All, Bit_SET);
-            GPIO_WriteBit(GPIOB, GPIO_Pin_All, Bit_SET);
-            GPIO_WriteBit(GPIOC, GPIO_Pin_All, Bit_SET);
-            GPIO_WriteBit(GPIOD, GPIO_Pin_All, Bit_SET);
-            GPIO_WriteBit(GPIOE, GPIO_Pin_All, Bit_SET);
-            GPIO_WriteBit(GPIOF, GPIO_Pin_All, Bit_SET);
-            GPIO_WriteBit(GPIOG, GPIO_Pin_All, Bit_SET);
-        }
+    //     // if (counter > 100000000 == 0)
+    //     {
+    //         GPIO_WriteBit(GPIOA, GPIO_Pin_All, Bit_SET);
+    //         GPIO_WriteBit(GPIOB, GPIO_Pin_All, Bit_SET);
+    //         GPIO_WriteBit(GPIOC, GPIO_Pin_All, Bit_SET);
+    //         GPIO_WriteBit(GPIOD, GPIO_Pin_All, Bit_SET);
+    //         GPIO_WriteBit(GPIOE, GPIO_Pin_All, Bit_SET);
+    //         GPIO_WriteBit(GPIOF, GPIO_Pin_All, Bit_SET);
+    //         GPIO_WriteBit(GPIOG, GPIO_Pin_All, Bit_SET);
+    //     }
     }
 }
